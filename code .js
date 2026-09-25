5efecbb4d7ddb35ace03963f52f7888193d3c05e
MernNasa <mernnasa@gmail.com>
2026-09-25 16:05:35 +0530

    happy

--- code .js
+++ code .js
@@ -9,6 +9,36 @@ function GCD(a,b){
 
 // GCD(12,18)
 
-function LCM(a,b){
-    
+
+function isHappyNumber(num) {
+        let seen = new Set();
+
+            while (num !== 1) {
+                    if (seen.has(num)) {
+                                return false;
+                                        }
+
+                                                seen.add(num);
+
+                                                        let sum = 0;
+
+                                                                while (num > 0) {
+                                                                            let digit = num % 10;
+                                                                                        sum += digit * digit;
+                                                                                                    num = Math.floor(num / 10);
+                                                                                                            }
+
+                                                                                                                    num = sum;
+                                                                                                                        }
+
+                                                                                                                            return true;
+                                                                                                                            }
+
+                                                                                                                            let n = 19;
+
+                                                                                                                            if (isHappyNumber(n)) {
+                                                                                                                                console.log(n + " is a Happy Number");
+                                                                                                                                } else {
+                                                                                                                                    console.log(n + " is not a Happy Number");
+                                                                                                                                    }
 }
